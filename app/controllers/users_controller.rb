8febf9be1072f958d2cf635a7fae79acc31afa26
motoken17 <tatetsu1017@gmail.com>
2020-08-25 01:34:00 +0000

    follow-view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
-  
+  def following
+      @user  = User.find(params[:id])
+      @users = @user.following_user
+      render 'show_follow'
+  end
+
+  def followers
+    @user  = User.find(params[:id])
+    @users = @user.follower_user
+    render 'show_follower'
+  end
   
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
   get 'pages/show'
-  resources :users, :only => [:show]
+  
   post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
   post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
-  
+  resources :users do
+    member do
+     get :following, :followers
+    end
+  end
+  resources :users, :only => [:show]
 end
